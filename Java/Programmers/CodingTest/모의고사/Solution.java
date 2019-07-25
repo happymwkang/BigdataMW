9b3245366345a039dbf85a747fd7abe68ee49d57
happymwkang <happymwkang@naver.com>
2019-07-25 23:15:11 +0900

    Programmers Coding Test Lev1 Complete [Algorithm]

--- Java/Programmers/CodingTest/모의고사/Solution.java
+++ Java/Programmers/CodingTest/모의고사/Solution.java
@@ -9,38 +9,40 @@ class Solution {
 		int score1 = 0;
 		int score2 = 0;
 		int score3 = 0;
+        int max_score = 0;
 
-		for (int i = 0; i < answer.length; i++) {
-				if (answer[i] == a1[i]) {
+		for (int i = 0; i < answers.length; i++) {
+				if (answers[i] == a1[i%5]) {
 					score1++;
 			}
 		}
 		// System.out.println("ddddddasdf         " + score1);
 		
-		for (int i = 0; i < answer.length; i++) {
-				if (answer[i] == a2[i]) {
+		for (int i = 0; i < answers.length; i++) {
+				if (answers[i] == a2[i%8]) {
 					score2++;
 			}
 		}
 		// System.out.println("als     " + score2);
 		
-		for (int i = 0; i < answer.length; i++) {
-					if (answer[i] == a3[i]) {
+		for (int i = 0; i < answers.length; i++) {
+					if (answers[i] == a3[i%10]) {
 						score3++;
 			}
 	
 		}
-		// 수정부분
-		  max_score = Math.max(score1, Math.max(score2, score3));
-        			List<Integer> list = new ArrayList<>();
-       			 if(max_score == score1) list.add(1);
-       			 if(max_score == score2) list.add(2);
-       			 if(max_score == score3) list.add(3);
+		// System.out.println("testest " + score3);
+
+    max_score = Math.max(score1, Math.max(score2, score3));
+        List<Integer> list = new ArrayList<>();
+        if(max_score == score1) list.add(1);
+        if(max_score == score2) list.add(2);
+        if(max_score == score3) list.add(3);
         
-      			  answer = new int[list.size()];
-     			   for(int i=0; i<list.size(); i++) {
-       			     answer[i] = (int) list.get(i);
-      				  return result;
-	}
-    
+        answer = new int[list.size()];
+        for(int i=0; i<list.size(); i++) {
+            answer[i] = (int) list.get(i);
+        }
+            return answer;
+   }
 }
