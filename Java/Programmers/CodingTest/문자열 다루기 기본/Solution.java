562aecc7881e52410ce3c53c50f99ad1d715826b
happymwkang <happymwkang@naver.com>
2019-08-14 01:12:10 +0900

    Programmers Coding Test Lv1 [Complete]

--- Java/Programmers/CodingTest/문자열 다루기 기본/Solution.java
+++ Java/Programmers/CodingTest/문자열 다루기 기본/Solution.java
@@ -1,15 +1,20 @@
 class Solution {
   public boolean solution(String s) {
         int result = 0;
+        int sLength = s.length();
 	      char[] test = {'1','2','3','4','5','6','7','8','9','0'};
+      
+      if(sLength == 4 || sLength == 6){
 	      for(int i = 0 ; i < s.length() ; i++) {
 	    	  for(int j =0 ; j < test.length ; j++) {
 	    		  if(s.charAt(i)==test[j]) {
 	    			  result++;
-	    		
-	    		  }
+	    		    }
 	    	  }
 	      }
+      }else {
+          return false;
+      }
 	      if(result == s.length()) {
 	    	  return true;
 	      }
